dbb676372fb3e43f568ae99fbdeaa37adff5a607
Khanh Vu <vtk.khanhvu@gmail.com>
2020-11-15 21:46:31 -0500

    Fix output format

--- dirTravel/dirTravel.py
+++ dirTravel/dirTravel.py
@@ -22,49 +22,55 @@ def receiveInput():
 		parser.error("[-] Specify a directory, use --help for more information")
 	return options.directory
 
-# Create a function to indent
-def indent(string,space):
-	result="  "*(space-1) +string 
-	return result 
-
-# The main function 	
+	
 def getDir():
 	path=receiveInput()
 	# Test if the path to the directory exist or not
 	# Run the FileInfo function if there exist the path 
 	if os.path.isdir(path)== True: 
-		# Create File list, dictionary
-		FileList=[]
-		FileDict={}
 	
 		# Initial counter for the file size and number of files 
 		TotalSize=0
 		TotalFile=0
-	
+
 		# Walk through the directory 
-		for root, dirs, files in os.walk(path):
-			# Indent the root according to how deep it is
-			print (indent(root,root.count(os.sep)))
-			# Link the file and its path and add them to the file list 
+		for current, dirs, files in os.walk(path):
+			DirSize=0
+			# Indent if the current directory is not the input directory
+			if current !=path:
+				spaces= 2*(1+ current.count(os.sep)-path.count(os.sep))
+			else:
+				spaces=0
+			indent= " "*spaces
+			print ("%s%s"%(indent,current))
+							
+			
+			# Running through the file lists
 			for file in files:
-				FileItem=os.path.join(root,file)
-				
-				# Get the size of the file 
+
+				# Join the file path and get file size
+				FileItem=os.path.join(current,file)
 				size=os.path.getsize(FileItem)
-				
-				
+								
 				# Adding up the total files and total size 
 				TotalFile+=1
 				TotalSize+=size 
-				
+				DirSize+=size
 				# Print out the indented string 
-				print ("%s\t%s bytes"%(indent("|-%s"%file,root.count(os.sep)), "{:,}".format(size))) 
+				print ("%s|-%s\t%s bytes"%(indent,file,"{:,}".format(size))) 
+			
+			# Return directory size if it's not the entered path
+			if current != path:
+				print ("%s[+] Directory size: %s bytes"%(indent,"{:,}".format(DirSize)))
+			else: 
+				continue
+				
 		print ("[+] The total files are: %s"%TotalFile)
 		print ("[+] The total file size is: %s bytes (%s MB)"%("{:,}".format(TotalSize),"{:,}".format(round(TotalSize/1048576,2))))
     # Return no directory if there is no such path 
 	else:
 		print ("[-] Directory does not exist")
 
-		
+# Execute the function		
 getDir()
 	
