b5f9b781e6d69fa8986d28e24913ea2f1f3db62b
Khanh Vu <31362665+UekiYukino@users.noreply.github.com>
2021-03-03 21:47:41 -0500

    Edit comment indentation

--- dirTravel/dirTravel.py
+++ dirTravel/dirTravel.py
@@ -8,6 +8,7 @@
 import os 
 import argparse
 
+
 def receiveInput():
     """This function accept input and return help menu when needed"""
     parser=argparse.ArgumentParser()
@@ -21,20 +22,19 @@ def receiveInput():
 def getDir():
     """Use the os.walk module of python to travel the directory and generate directory tree report"""
     path=receiveInput()
-	# Test if the path to the directory exist or not
-	# Run the FileInfo function if there exist the path 
+    # Test if the path to the directory exist or not
+    # Run the FileInfo function if there exist the path 
     if os.path.isdir(path)== True: 
-	
-		# Initial counter for the file size and number of files 
+        # Initial counter for the file size and number of files 
         TotalSize=0
         TotalFile=0
 
-		# Walk through the directory 
+        # Walk through the directory 
         for current, dirs, files in os.walk(path):
             DirSize=0
-			# Indent if the current directory is not the input directory
+            # Indent if the current directory is not the input directory
             if current !=path:
-                #Indent according to the amount of depth in relative to the os path
+                    #Indent according to the amount of depth in relative to the os path
 	            spaces= 2*(1+ current.count(os.sep)-path.count(os.sep))
             else:
 	            spaces=0
@@ -46,25 +46,22 @@ def getDir():
                 print("{}/{}/".format(indent,break_dir[len(break_dir)-1]))
             else:
                 print(path)
-
 							
-			# Running through the file lists
+            # Running through the file lists
             for file in files:
-
-				# Join the file path and get file size
+                # Join the file path and get file size
                 FileItem=os.path.join(current,file)
                 try:
                     size=os.path.getsize(FileItem)
                 except:
                     continue
-				# Adding up the total files and total size 
+                # Adding up the total files and total size 
                 TotalFile+=1
                 TotalSize+=size 
                 DirSize+=size
-				# Print out the indented string 
+                # Print out the indented string 
                 print ("{}|-{}\t{:,} bytes".format(indent,file,size)) 
-			
-				
+					
         print ("[+] The total files are: {}".format(TotalFile))
         print ("[+] The total file size is: {:,} bytes ({:,.2f} MB)".format(TotalSize,TotalSize/1048576))
     # Return no directory if there is no such path 
